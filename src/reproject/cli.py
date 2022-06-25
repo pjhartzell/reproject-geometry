16c3f5e21e254e8801f819a26bb1880a2b227775
pjhartzell <preston.hartzell@gmail.com>
2022-06-25 07:16:00 -0400

    update readme

--- src/reproject/cli.py
+++ src/reproject/cli.py
@@ -12,22 +12,22 @@ from reproject.reproject import DEFAULT_PRECISION, reproject_geometry
 @click.argument("SRC_CRS")
 @click.argument("DST_CRS")
 @click.option(
+    "-t", "--tolerance", help="Destination geometry error tolerance", type=float
+)
+@click.option(
     "-p",
     "--precision",
     help="Number of decimal places in output coords",
     type=int,
     default=DEFAULT_PRECISION,
 )
-@click.option(
-    "-t", "--tolerance", help="Destination geometry error tolerance", type=float
-)
 @click.option("-o", "--outfile", help="File path for reprojected geojson")
 def cli(
     infile: str,
     src_crs: str,
     dst_crs: str,
-    precision: int = DEFAULT_PRECISION,
     tolerance: Optional[float] = None,
+    precision: int = DEFAULT_PRECISION,
     outfile: Optional[str] = None,
 ) -> None:
     """Reprojects a Polygon in a GeoJSON file.
@@ -37,13 +37,15 @@ def cli(
         infile (str): Path to file containing a GeoJSON Polygon
         src_crs (str): Source CRS string, e.g., an EPSG code or WKT
         dst_crs (str): Destination CRS string
-        dst_tolerance (float): Maximum acceptable "error" of the reprojected
-            geometry in the destination CRS linear unit.
-        outfile (str, optional): GeoJSON file path for the reprojected geometry.
-            If not specified, the file will be saved alongside the INFILE.
+        dst_tolerance (float, optional): Maximum acceptable "error" of the
+            reprojected geometry in the destination CRS linear unit. If not
+            specified, the source geometry will be reprojected without any
+            densification.
         precision (int, optional): The number of decimal places to include in
             the coordinates for the reprojected geometry. Defaults to 3 decimal
             places.
+        outfile (str, optional): GeoJSON file path for the reprojected geometry.
+            If not specified, the file will be saved alongside the INFILE.
     """
     with open(infile, "r") as filein:
         geometry = json.load(filein)
